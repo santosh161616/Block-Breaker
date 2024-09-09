2a76e244fe7b9f427357457c5bbca55d2eba822e
santosh161616 <santosh161616@gmail.com>
2024-09-10 00:40:56 +0530

    Made Some changes in GameEnd Scene

--- Assets/Scripts/GameCredits.cs
+++ Assets/Scripts/GameCredits.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameCredits : MonoBehaviour
 {
     [SerializeField] TMP_Text gameCredit;
+    [SerializeField] private Button playButton;
     // Start is called before the first frame update
     void Start()
     {
+        playButton.onClick.AddListener(LoadGame);
         GameCredit();
     }
 
@@ -16,5 +20,11 @@ public class GameCredits : MonoBehaviour
     {
         string credit = "Congratulations! \r\nYou’ve completed all the levels!\r\n\r\nThank you for playing <color=red><b>BRICK BOUNCER</b></color>! Your dedication means a lot, and I hope you enjoyed the experience!\r\n\r\nCredits:\r\nGame Development, Design, Art, Sound & Music: \r\n\n\"<color=red>Santosh Kumar</color>\"\r\n\r\nStay tuned for more games and updates. Don’t forget to share your thoughts and feedback! ";
         gameCredit.text = credit;
+    }
+
+    public void LoadGame()
+    {
+        string sceneToLoad = StaticUrlScript.Dashboard;
+        SceneManager.LoadSceneAsync(sceneToLoad);
     }
 }
--- Assets/Scripts/StaticUrlScript.cs
+++ Assets/Scripts/StaticUrlScript.cs
@@ -7,8 +7,10 @@ public static class StaticUrlScript
     //PlayerPrefs
     public static string highScore = "HighScore";
     public static string currentLevel = "CurrentLevel";
-    public static string GameOver = "03 Game Over";
 
+    //GameScene
+    public static string GameOver = "03 Game Over";
+    public static string Dashboard = "01 Start Menu";
     //Firebase
     public static string StartGame_Firebase = "StartGame";
     public static string NextLevel_Firebase = "NextLevel";
